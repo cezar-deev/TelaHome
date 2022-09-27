cdc7be1aae2b6d48dac05a093ca8450cea4a5fcf
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-27 17:10:15 -0300

    OK

--- others/CalculoPesoSpool/JS/scriptSpool.js
+++ others/CalculoPesoSpool/JS/scriptSpool.js
@@ -5,8 +5,15 @@ function calcular() {
 var comprimento1 = document.getElementById('comp1').value;
 var peso1 = document.getElementById('peso1').value;
 
-var calculo = parseInt(comprimento1) / parseInt(peso1)
+var comprimento2 = document.getElementById('comp2').value;
+var peso2 = document.getElementById('peso2').value;
 
-document.getElementById('res1').innerHTML = calculo;
+var calculo1 = parseInt(comprimento1) / parseInt(peso1)
+var calculo2 = parseInt(comprimento2) / parseInt(peso2)
+var total = calculo1 + calculo2
+
+document.getElementById('res1').innerHTML = calculo1;
+document.getElementById('res2').innerHTML = calculo2;
+document.getElementById('res3', "kg").innerHTML = total;
 
 }
