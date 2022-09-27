17055768befa651640b29662249d6ad4d84937b2
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-27 17:37:17 -0300

    ok

--- others/CalculoPesoSpool/JS/scriptSpool.js
+++ others/CalculoPesoSpool/JS/scriptSpool.js
@@ -1,10 +1,8 @@
 
-console.log("cezar")
-
 function calcular() {
+    
 var comprimento1 = document.getElementById('comp1').value;
 var peso1 = document.getElementById('peso1').value;
-
 var comprimento2 = document.getElementById('comp2').value;
 var peso2 = document.getElementById('peso2').value;
 
@@ -15,5 +13,17 @@ var total = calculo1 + calculo2
 document.getElementById('res1').innerHTML = calculo1;
 document.getElementById('res2').innerHTML = calculo2;
 document.getElementById('res3', "kg").innerHTML = total;
+
+}
+
+function verificarZero () {
+
+    var comp1 = document.getElementById('comp1').value;
+
+    if(comp1<1) {
+        
+    }else {
+
+    }
 
 }
