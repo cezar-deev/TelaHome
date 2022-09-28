9508312ddd201a8348a8d615d13962580befdb59
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-28 17:38:52 -0300

    ok

--- others/CalculoPesoSpool/JS/scriptSpool.js
+++ others/CalculoPesoSpool/JS/scriptSpool.js
@@ -4,7 +4,7 @@ function calcPincipal () {
     var comprimento1 = document.getElementById('comp1').value;
     var peso1 = document.getElementById('peso1').value;
 
-    var total1 = parseFloat(comprimento1) / parseFloat(peso1);
+    var total1 = parseFloat(peso1) / parseFloat(comprimento1);
 
     if(comprimento1<1){
         alert("Comprimento nao pode ser menor que 1mm ")
@@ -12,14 +12,14 @@ function calcPincipal () {
         alert("Peso não pode ser menor ou igual a zero ")
     }
 
-    document.getElementById('res1').innerHTML = total1.toFixed(1)+" Kg";
+    document.getElementById('res1').innerHTML = total1.toFixed(3)+" Kg";
 }
 
 function calcDerivacao () {
     var comprimento2 = document.getElementById('comp2').value;
     var peso2 = document.getElementById('peso2').value;
 
-    var total2 = parseFloat(comprimento2) / parseFloat(peso2);
+    var total2 = parseFloat(peso2)/parseFloat(comprimento2);
 
     if(comprimento2<1){
         alert("Comprimento nao pode ser menor que 1mm ")
@@ -27,7 +27,7 @@ function calcDerivacao () {
         alert("Peso não pode ser menor ou igual a zero ")
     }
 
-    document.getElementById('res2').innerHTML = total2.toFixed(1)+" Kg";
+    document.getElementById('res2').innerHTML = total2.toFixed(3)+" Kg";
 }
 
 function calcPesoTot() {
@@ -37,8 +37,8 @@ function calcPesoTot() {
     var comprimento2 = document.getElementById('comp2').value;
     var peso2 = document.getElementById('peso2').value;
     
-    var calculo1 = parseFloat(comprimento1) / parseFloat(peso1);
-    var calculo2 = parseFloat(comprimento2) / parseFloat(peso2);
+    var calculo1 = parseFloat(peso1) / parseFloat(comprimento1);
+    var calculo2 = parseFloat(peso2) / parseFloat(comprimento2);
     
     var total = calculo1 + calculo2
 
@@ -46,8 +46,8 @@ function calcPesoTot() {
         alert("Insira uma valor ")
     } 
     
-    document.getElementById('res1').innerHTML = calculo1.toFixed(1)+" Kg";
-    document.getElementById('res2').innerHTML = calculo2.toFixed(1)+" Kg";
-    document.getElementById('res3', "kg").innerHTML = total.toFixed(1)+" Kg";
+    document.getElementById('res1').innerHTML = calculo1.toFixed(3)+" Kg";
+    document.getElementById('res2').innerHTML = calculo2.toFixed(3)+" Kg";
+    document.getElementById('res3', "kg").innerHTML = total.toFixed(3)+" Kg";
          
 }
