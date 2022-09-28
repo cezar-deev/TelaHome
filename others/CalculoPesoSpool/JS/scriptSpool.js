8544723d09936ca05b81543310785cb67b204532
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-28 15:22:19 -0300

    ok

--- others/CalculoPesoSpool/JS/scriptSpool.js
+++ others/CalculoPesoSpool/JS/scriptSpool.js
@@ -1,42 +1,56 @@
 
 
-function calcular() {
-    
-verificarZero ()
-verificarvazio ()
+function calcPincipal () {
+    var comprimento1 = document.getElementById('comp1').value;
+    var peso1 = document.getElementById('peso1').value;
 
-var comprimento1 = document.getElementById('comp1').value;
-var peso1 = document.getElementById('peso1').value;
-var comprimento2 = document.getElementById('comp2').value;
-var peso2 = document.getElementById('peso2').value;
+    var total1 = parseFloat(comprimento1) / parseFloat(peso1);
 
-var calculo1 = parseInt(comprimento1) / parseInt(peso1);
-var calculo2 = parseInt(comprimento2) / parseInt(peso2);
-
-var total = calculo1 + calculo2
-
-document.getElementById('res1').innerHTML = calculo1.toFixed(3);
-document.getElementById('res2').innerHTML = calculo2.toFixed(3);
-document.getElementById('res3', "kg").innerHTML = total.toFixed(3)
-
-
-    function verificarZero () {
-        var comp1 = document.getElementById('comp1').value
-        if(comp1==0 || peso1==0) {
-            alert("Insira uma valor diferente de zero!")
-        }
-
+    if(comprimento1<1){
+        alert("Comprimento nao pode ser menor que 1mm ")
+    } if (peso1<=0){
+        alert("Peso não pode ser menor ou igual a zero ")
     }
 
-    function verificarvazio () {
-
-        var comp1 = document.getElementById('comp1').value
-        if(comp1 =="" || peso1=="") {
-            alert("Insira um valor")
-        }
-
-    }
-
-
+    document.getElementById('res1').innerHTML = total1.toFixed(1)+" Kg";
 }
 
+function calcDerivacao () {
+    var comprimento2 = document.getElementById('comp2').value;
+    var peso2 = document.getElementById('peso2').value;
+
+    var total2 = parseFloat(comprimento2) / parseFloat(peso2);
+
+    if(comprimento2<1){
+        alert("Comprimento nao pode ser menor que 1mm ")
+    } if (peso2<=0){
+        alert("Peso não pode ser menor ou igual a zero ")
+    }
+
+    document.getElementById('res2').innerHTML = total2.toFixed(1)+" Kg";
+}
+
+function calcPesoTot() {
+
+    var comprimento1 = document.getElementById('comp1').value;
+    var peso1 = document.getElementById('peso1').value;
+    var comprimento2 = document.getElementById('comp2').value;
+    var peso2 = document.getElementById('peso2').value;
+    
+    var calculo1 = parseFloat(comprimento1) / parseFloat(peso1);
+    var calculo2 = parseFloat(comprimento2) / parseFloat(peso2);
+    
+    var total = calculo1 + calculo2
+
+    if (comprimento1=="" || peso1=="" || comprimento2=="" || peso2=="" ) {
+        alert("Insira uma valor ")
+    } 
+    
+    document.getElementById('res1').innerHTML = calculo1.toFixed(1)+" Kg";
+    document.getElementById('res2').innerHTML = calculo2.toFixed(1)+" Kg";
+    document.getElementById('res3', "kg").innerHTML = total.toFixed(1)+" Kg";
+    
+    
+            
+        
+}
